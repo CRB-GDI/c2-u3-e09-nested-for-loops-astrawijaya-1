c9c2417c33fcf30d651f1fcc2a3d89c800cb715c
Mr. Wijaya <astra.wijaya@neric.org>
2024-03-06 11:19:37 -0500

    finished exercise 9

---
+++ Exercise9/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercise9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your row width: ");
+            int width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter how many rows: ");
+            int height = int.Parse(Console.ReadLine());
+
+            //print rows going down
+            Console.WriteLine();
+            for (int a =1; a <= height; a++)
+            {
+                //print content of the row to the right
+                for (int b =1; b <= width; b++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
